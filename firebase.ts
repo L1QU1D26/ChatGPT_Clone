33a93e4726241e58978f0f00cd14d611ee9245d7
L1QU1D26 <102826893+L1QU1D26@users.noreply.github.com>
2025-05-26 13:01:53 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,11 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxYS-5NF44L0BOZpJaKocjywSPYo_JFJc",
-  authDomain: "chatgpt-clone-6509f.firebaseapp.com",
-  projectId: "chatgpt-clone-6509f",
-  storageBucket: "chatgpt-clone-6509f.firebasestorage.app",
-  messagingSenderId: "252567696363",
-  appId: "1:252567696363:web:f0d38ecd294a4345921fae"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 
